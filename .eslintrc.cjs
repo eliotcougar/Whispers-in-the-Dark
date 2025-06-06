b81d1304d07ed7dabc3e14014b427ae66f462adf
eliotcougar <eliotfur@gmail.com>
2025-06-06 11:45:18 +0300

    Add ESLint setup and integrate into test

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  ignorePatterns: ['node_modules', 'dist', 'build'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+};
