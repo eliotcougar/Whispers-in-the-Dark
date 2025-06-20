4d4bfa154ab622ed257955a43ecd2955e8ec6862
eliotcougar <eliotfur@gmail.com>
2025-06-21 00:01:26 +0300

    Swapped Companion and NearbyNPC icons

--- components/elements/icons/Companion.tsx
+++ components/elements/icons/Companion.tsx
@@ -7,7 +7,11 @@ function SvgCompanion({ height = '1em', width = '1em' }: SVGProps<SVGSVGElement>
     width={width}
     xmlns="http://www.w3.org/2000/svg"
           >
-    <path d="M13 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0m5 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-4 7a4 4 0 0 0-8 0v3h8zM6 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0m10 10v-3a6 6 0 0 0-.75-2.906A3.005 3.005 0 0 1 19 15v3zM4.75 12.094A6 6 0 0 0 4 15v3H1v-3a3 3 0 0 1 3.75-2.906" />
+    <path
+      clipRule="evenodd"
+      d="M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-7 9a7 7 0 1 1 14 0z"
+      fillRule="evenodd"
+    />
   </svg>)
 }
 export default SvgCompanion;
