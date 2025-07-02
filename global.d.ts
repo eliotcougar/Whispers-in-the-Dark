f979d7d1b634d57a6e42829c1f74255f5748b490
eliotcougar <eliotfur@gmail.com>
2025-07-02 22:54:52 +0300

    Integrate Google Identity Services

---
+++ global.d.ts
@@ -0,0 +1,28 @@
+interface GoogleTokenClient {
+  requestAccessToken: () => void;
+}
+
+interface GoogleOauth2 {
+  initTokenClient: (config: {
+    client_id: string;
+    scope: string;
+    callback: (resp: { access_token?: string; error?: string }) => void;
+  }) => GoogleTokenClient;
+}
+
+interface GoogleAccounts {
+  oauth2?: GoogleOauth2;
+}
+
+interface GoogleNamespace {
+  accounts?: GoogleAccounts;
+}
+
+declare global {
+  interface Window {
+    google?: GoogleNamespace;
+    GOOGLE_CLIENT_ID?: string;
+  }
+}
+
+export {};
