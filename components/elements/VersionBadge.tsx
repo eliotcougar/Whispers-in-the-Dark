d6a4bac2a411f440cefa507ec9c5116793db52e0
eliotcougar <eliotfur@gmail.com>
2025-07-03 20:24:48 +0300

    Show API key source in title menu

--- components/elements/VersionBadge.tsx
+++ components/elements/VersionBadge.tsx
@@ -1,16 +1,19 @@
 interface VersionBadgeProps {
   readonly version: string;
+  readonly sourceInfo?: string;
 }
 
-function VersionBadge({ version}: VersionBadgeProps) {
+function VersionBadge({ version, sourceInfo }: VersionBadgeProps) {
   return (
     <span className='text-sm text-slate-300 absolute bottom-4 right-4'>
       {'Version: ' + version}
+      {sourceInfo ? ` – ${sourceInfo}` : null}
     </span>
   );
 }
 
 VersionBadge.defaultProps = {
+  sourceInfo: undefined,
 };
 
 export default VersionBadge;
