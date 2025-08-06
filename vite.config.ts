62599484c83c31300ad52e3aa9f14162a0e2bc7d
Eliot the Cougar <eliotfur@gmail.com>
2025-08-06 16:23:00 +0300

    restore some chunking

--- vite.config.ts
+++ vite.config.ts
@@ -36,5 +36,28 @@ export default defineConfig(({ mode }: { mode: string }) => {
         '@': path.resolve(__dirname, '.'),
       },
     },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('genai')) {
+              return 'gemini';
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+            if (id.includes('debug')) {
+              return 'debug';
+            }
+            if (id.includes('resources')) {
+              return 'resources';
+            }
+            if (id.includes('hooks')) {
+              return 'hooks';
+            }
+          },
+        },
+      },
+    },
   };
 });
